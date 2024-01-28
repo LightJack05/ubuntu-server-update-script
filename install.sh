1016a8bd582a6774639dc4f1a209d9fb10ff2c9e
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2024-01-28 14:22:16 +0100

    Added install script

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
-echo "Creating /srv/update-script"
+echo "Creating /srv/update-script..."
 mkdir /srv/update-script
 
+echo "Downloading update script..."
+wget https://raw.githubusercontent.com/LightJack05/ubuntu-server-update-script/ -o /srv/update-script/update.sh
+chmod +x /srv/update-script/update.sh
+
+echo "Creating cronjob for current user..."
+(crontab -l 2>/dev/null; echo "10 3 * * 1 /srv/update-script/update.sh") | crontab -
+
+echo "Completed setup. Script can be run manually using: /srv/update-script/update.sh"
